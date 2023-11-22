6b42a4240b7e0225f7ffa4631fc159bdef1f1998
Joan Marti <joanmartigamez@gmail.com [core]>
2023-11-22 09:42:13 +0100

    adding files

---
+++ src/pages/index.jsx
@@ -0,0 +1,25 @@
+import { useAppContext } from "../store/store";
+import Layout from "../components/layout";
+import Book from "../components/book";
+
+function Index() {
+    const store = useAppContext();
+
+    const booksContainer = {
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "10px"
+    };
+
+    return (
+        <Layout>
+            <div style={booksContainer}>
+                {store.items.map((item) => (
+                    <Book key={item.id} item={item} />
+                ))}
+            </div>
+        </Layout>
+    );
+};
+
+export default Index;
